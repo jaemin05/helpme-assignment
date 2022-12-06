68435f3c7245dbd2387373054ce963c1d9bf8fc3
jaemin <sonjm3518@dsm.hs.kr>
2022-12-06 20:29:18 +0900

    project: default

---
+++ helpme-assignment/src/test/java/com/example/helpmeassignment/HelpmeAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helpmeassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpmeAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
