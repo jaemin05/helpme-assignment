0fbd319c4a48f59cc452c47de178ed1b62effa43
jaemin <sonjm3518@dsm.hs.kr>
2022-12-12 11:24:24 +0900

    feat: Exception Custom

---
+++ src/main/java/com/example/helpmeassignment/global/error/exception/CustomException.java
@@ -0,0 +1,16 @@
+package com.example.helpmeassignment.global.error.exception;
+
+import com.example.helpmeassignment.global.error.ErrorProperty;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomException extends RuntimeException {
+    private final ErrorProperty errorProperty;
+
+    @Override
+    public synchronized Throwable fillInStackTrace() {
+        return this;
+    }
+}
