00ba2d0061246c5c6a55e30cbdaf0bac61801f6b
jaemin <sonjm3518@dsm.hs.kr>
2022-12-12 11:19:41 +0900

    feat: Security Filter 앞에 위치

---
+++ src/main/java/com/example/helpmeassignment/global/security/FilterConfig.java
@@ -0,0 +1,25 @@
+package com.example.helpmeassignment.global.security;
+
+import com.example.helpmeassignment.global.filter.ExceptionFilter;
+import com.example.helpmeassignment.global.security.jwt.JwtTokenFilter;
+import com.example.helpmeassignment.global.security.jwt.JwtTokenParser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+public class FilterConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    private final JwtTokenParser jwtTokenParser;
+
+    @Override
+    public void configure(HttpSecurity http) {
+        JwtTokenFilter jwtTokenFilter = new JwtTokenFilter(jwtTokenParser);
+        ExceptionFilter exceptionFilter = new ExceptionFilter();
+
+        http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(exceptionFilter, JwtTokenFilter.class);
+    }
+}
