e08945a48f1ebe5a6caf1cb2fec004fbf183dd3e
jaemin <sonjm3518@dsm.hs.kr>
2022-12-12 11:28:22 +0900

    feat: Properties 관련 Config

---
+++ src/main/java/com/example/helpmeassignment/global/config/PropertiesConfig.java
@@ -0,0 +1,10 @@
+package com.example.helpmeassignment.global.config;
+
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
+
+@ConfigurationPropertiesScan("com.example.helpmeassignment")
+@Configuration
+public class PropertiesConfig {
+}
