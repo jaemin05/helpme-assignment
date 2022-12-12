bfe72d87c814f85af103bf796d85532d58b79503
jaemin <sonjm3518@dsm.hs.kr>
2022-12-12 11:25:06 +0900

    feat: Spring Security Config - 접근 설정 등

---
+++ src/main/java/com/example/helpmeassignment/global/security/SecurityConfig.java
@@ -0,0 +1,63 @@
+package com.example.helpmeassignment.global.security;
+
+import com.example.helpmeassignment.global.security.jwt.JwtTokenFilter;
+import com.example.helpmeassignment.global.security.jwt.JwtTokenParser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@Configuration
+public class SecurityConfig {
+
+    private final JwtTokenParser jwtTokenParser;
+
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .formLogin().disable()
+                .csrf().disable()
+                .cors();
+
+        http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http
+                .authorizeHttpRequests()
+                .requestMatchers(HttpMethod.POST, "/admin").permitAll()
+                .requestMatchers(HttpMethod.POST, "/admin/login").permitAll()
+
+                .requestMatchers(HttpMethod.POST, "/member").authenticated()
+                .requestMatchers(HttpMethod.POST, "/member/{member-id}").authenticated()
+
+                .requestMatchers(HttpMethod.POST, "/deposit").authenticated()
+                .requestMatchers(HttpMethod.GET, "/deposit").authenticated()
+                .requestMatchers(HttpMethod.GET, "/deposit/{member-id}").authenticated()
+                .requestMatchers(HttpMethod.GET, "/deposit/total").authenticated()
+                .requestMatchers(HttpMethod.GET, "/deposit/time").authenticated()
+
+                .requestMatchers(HttpMethod.POST, "/use").authenticated()
+                .requestMatchers(HttpMethod.GET, "/use").authenticated()
+                .requestMatchers(HttpMethod.GET, "/use/time").authenticated()
+
+                .anyRequest().authenticated();
+
+        http
+                .apply(new FilterConfig(jwtTokenParser));
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
