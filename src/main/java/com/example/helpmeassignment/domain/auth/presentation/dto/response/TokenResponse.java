b36bb296aac2c87ac258610a889834cfe7a61ac2
jaemin <sonjm3518@dsm.hs.kr>
2022-12-12 11:10:22 +0900

    feat: 토큰 반환

---
+++ src/main/java/com/example/helpmeassignment/domain/auth/presentation/dto/response/TokenResponse.java
@@ -0,0 +1,13 @@
+package com.example.helpmeassignment.domain.auth.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TokenResponse {
+
+    private final String accessToken;
+
+    private final String refreshToken;
+}
